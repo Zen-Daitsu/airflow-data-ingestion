f4e8213c890e1f3a8fc4f6e46bde17ab86aa8916
Amine Kraiem <amine.krayem@gmail.com>
2026-06-21 23:39:48 -0400

    Update import and variable names in task_print_list.py

--- tasks/task_print_list.py
+++ tasks/task_print_list.py
@@ -2,16 +2,16 @@
 #Date : 05/02/2025
 #Examen final - Collecte de données
 
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 
 def print_list():
-    my_list = ['Flouflou', 'Flouclair', 'CLAiCLAIR']
-    for item in my_list:
+    items = ['Flouflou', 'Flouclair', 'CLAiCLAIR']
+    for item in items:
         print(item)
 
 def create_print_list_task(dag):
     return PythonOperator(
         task_id='print_list',
         python_callable=print_list,
-        dag=dag,
+        dag=dag
     )
