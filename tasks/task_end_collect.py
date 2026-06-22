f21b3ed78704bbbf9368987ca45f840adade399f
Amine Kraiem <amine.krayem@gmail.com>
2026-06-21 23:40:31 -0400

    Update end collect task message to English

--- tasks/task_end_collect.py
+++ tasks/task_end_collect.py
@@ -7,6 +7,6 @@ from airflow.operators.bash import BashOperator
 def create_end_collect_task(dag):
     return BashOperator(
         task_id='end_collect',
-        bash_command='echo "Fin de la collecte de données"',
-        dag=dag,
+        bash_command='echo "Collection ended."',
+        dag=dag
     )
